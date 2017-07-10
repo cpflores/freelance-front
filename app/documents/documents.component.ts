e3f180a157d63b46e3c1c14c83fc6dc56665ad3a
Chadwick Flores <chadwickflores@gmail.com>
2017-07-10 13:38:51 -0500

    updated the Docs design with cards

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent {
       description: 'asdfasdfasdf asdfasdfasdf',
       file_url: 'http://google.com',
       updated_at: '06/21/17',
-      image_url: 'http://google.com'
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     },
     {
       title: "My Second Doc",
       description: 'asdfasdfasdf asdfasdfasdf',
       file_url: 'http://google.com',
       updated_at: '06/21/17',
-      image_url: 'http://google.com'
+      image_url: 'https://c1.staticflickr.com/3/2842/33161286816_5550668958_b.jpg'
     },
     {
       title: "My Last Doc",
       description: 'asdfasdfasdf asdfasdfasdf',
       file_url: 'http://google.com',
       updated_at: '06/21/17',
-      image_url: 'http://google.com'
+      image_url: 'https://c1.staticflickr.com/4/3939/33067431515_4b19aed1e9_b.jpg'
     }
   ]
 }
