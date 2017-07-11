c5c21b891ac4bd7b6b27b6156889f83ad476c037
Chadwick Flores <chadwickflores@gmail.com>
2017-07-11 14:30:35 -0500

    added observable to document components and pulled in live api data from document serivce

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ],
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
+  documents: Document[];
+  errorMessage: string;
+  mode: "Observable";
 
-  documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: 'asdfasdfasdf asdfasdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '06/21/17',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
-    },
-    {
-      title: "My Second Doc",
-      description: 'asdfasdfasdf asdfasdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '06/21/17',
-      image_url: 'https://c1.staticflickr.com/3/2842/33161286816_5550668958_b.jpg'
-    },
-    {
-      title: "My Last Doc",
-      description: 'asdfasdfasdf asdfasdfasdf',
-      file_url: 'http://google.com',
-      updated_at: '06/21/17',
-      image_url: 'https://c1.staticflickr.com/4/3939/33067431515_4b19aed1e9_b.jpg'
-    }
-  ]
+  constructor(
+    private documentService: DocumentService
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
